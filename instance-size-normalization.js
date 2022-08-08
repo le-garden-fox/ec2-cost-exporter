aa48344921ce3a15a341ed740cadec95b8f87110
Angel <angelrob.flores@gmail.com>
2022-08-08 11:16:28 -0600

    Aggregate ec2 data

---
+++ instance-size-normalization.js
@@ -0,0 +1,24 @@
+module.exports = {
+  INSTANCE_SIZE_NORMALIZATION_DATA: {
+    "nano": 0.25,
+    "micro": 0.5,
+    "small": 1,
+    "medium": 2,
+    "large": 4,
+    "xlarge": 8,
+    "2xlarge": 16,
+    "3xlarge": 24,
+    "4xlarge": 32,
+    "6xlarge": 48,
+    "8xlarge": 64,
+    "9xlarge": 72,
+    "10xlarge": 80,
+    "12xlarge": 96,
+    "16xlarge": 128,
+    "18xlarge": 144,
+    "24xlarge": 192,
+    "32xlarge": 256,
+    "56xlarge": 448,
+    "112xlarge": 896,
+  }
+};
